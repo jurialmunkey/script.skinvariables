b4bf342b188c1903de1c418b17e675c7c7ff546d
jurialmunkey <jurialmunkey@kodi.tv>
2020-04-24 10:55:49 +1000

    first commit

---
+++ resources/lib/script.py
@@ -0,0 +1,91 @@
+# -*- coding: utf-8 -*-
+# Module: default
+# Author: jurialmunkey
+# License: GPL v.3 https://www.gnu.org/copyleft/gpl.html
+import sys
+import xbmc
+import xbmcgui
+import xbmcvfs
+from json import loads
+import resources.lib.utils as utils
+
+
+class Script(object):
+    def __init__(self):
+        super(Script, self).__init__()
+        self.home = xbmcgui.Window(10000)
+        self.params = {}
+
+    def get_params(self):
+        for arg in sys.argv:
+            if arg == 'script.py':
+                pass
+            elif '=' in arg:
+                arg_split = arg.split('=', 1)
+                if arg_split[0] and arg_split[1]:
+                    key, value = arg_split
+                    self.params.setdefault(key, value)
+            else:
+                self.params.setdefault(arg, True)
+
+    def make_variables(self):
+        try:
+            vfs_file = xbmcvfs.File('special://skin/shortcuts/skin-variables.json')
+            content = vfs_file.read()
+            meta = loads(content) or []
+        finally:
+            vfs_file.close()
+
+        if not self.params.get('force_build'):  # Allow overriding over built check
+            this_version = len(content)
+            last_version = utils.try_parse_int(xbmc.getInfoLabel('Skin.String(script-skinvariables-hash)'))
+            if this_version and last_version and this_version == last_version:
+                return  # Already updated
+
+        if not meta:
+            return
+
+        txt = '<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<includes>'
+        for variable in meta:
+            v_name = variable.get('name')
+            values = variable.get('values')
+            if not v_name or not values:
+                continue  # Skip items without names or values to make
+            containers = variable.get('containers', [])
+            containers.append('')
+            li_a = variable.get('listitems', {}).get('start', 0)
+            li_z = variable.get('listitems', {}).get('end')
+            listitems = [i for i in range(li_a, int(li_z) + 1)] if li_z else []
+            listitems.append('')
+            for container in containers:
+                for listitem in listitems:
+                    txt += '\n    <variable name=\"{}'.format(v_name)
+                    txt += '_C{}'.format(container) if container else ''
+                    txt += '_{}'.format(listitem) if listitem or listitem == 0 else ''
+                    txt += '\">'
+                    for value in values:
+                        for k, v in value.items():
+                            if not k or not v:
+                                continue
+                            li_name = 'Container({}).ListItem'.format(container) if container else 'ListItem'
+                            li_name += '({})'.format(listitem) if listitem else ''
+                            f_dict = {
+                                'listitem': li_name,
+                                'listitemabsolute': li_name.replace('ListItem(', 'ListItemAbsolute('),
+                                'listitemnowrap': li_name.replace('ListItem(', 'ListItemNoWrap('),
+                                'listitemposition': li_name.replace('ListItem(', 'ListItemPosition(')}
+                            cond = k.format(**f_dict)
+                            valu = v.format(**f_dict)
+                            txt += '\n        <value condition=\"{}\">{}</value>'.format(cond, valu)
+                    txt += '\n    </variable>'
+        txt += '\n</includes>'
+
+        filepath = 'special://skin/{}/script-skinvariables-includes.xml'.format(self.params.get('make_variables'))
+        f = xbmcvfs.File(filepath, 'w')
+        f.write(utils.try_encode_string(txt))
+        f.close()
+        xbmc.executebuiltin('ReloadSkin()')
+        xbmc.executebuiltin('Skin.SetString(script-skinvariables-hash,{})'.format(len(content)))
+
+    def router(self):
+        self.make_variables()
---
+++ resources/lib/utils.py
@@ -0,0 +1,64 @@
+import sys
+import xbmc
+import xbmcvfs
+import xbmcaddon
+_addonlogname = '[script.skinvariables]\n'
+_addon = xbmcaddon.Addon()
+
+
+def makepath(path):
+    if xbmcvfs.exists(path):
+        return xbmc.translatePath(path)
+    xbmcvfs.mkdirs(path)
+    return xbmc.translatePath(path)
+
+
+def try_parse_int(string):
+    '''helper to parse int from string without erroring on empty or misformed string'''
+    try:
+        return int(string)
+    except Exception:
+        return 0
+
+
+def try_parse_float(string):
+    '''helper to parse float from string without erroring on empty or misformed string'''
+    try:
+        return float(string or 0)
+    except Exception:
+        return 0
+
+
+def try_decode_string(string, encoding='utf-8'):
+    """helper to decode strings for PY 2 """
+    if sys.version_info.major == 3:
+        return string
+    try:
+        return string.decode(encoding)
+    except Exception:
+        return string
+
+
+def try_encode_string(string, encoding='utf-8'):
+    """helper to encode strings for PY 2 """
+    if sys.version_info.major == 3:
+        return string
+    try:
+        return string.encode(encoding)
+    except Exception:
+        return string
+
+
+def kodi_log(value, level=0):
+    try:
+        if isinstance(value, bytes):
+            value = value.decode('utf-8')
+        logvalue = u'{0}{1}'.format(_addonlogname, value)
+        if sys.version_info < (3, 0):
+            logvalue = logvalue.encode('utf-8', 'ignore')
+        elif level == 1:
+            xbmc.log(logvalue, level=xbmc.LOGNOTICE)
+        else:
+            xbmc.log(logvalue, level=xbmc.LOGDEBUG)
+    except Exception as exc:
+        xbmc.log(u'Logging Error: {}'.format(exc), level=xbmc.LOGNOTICE)
