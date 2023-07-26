016f3e29fea68e0f514b8cf4927995600eb4c2c0
jurialmunkey <jurialmunkey@kodi.tv>
2023-07-26 13:12:57 +1000

    :broom: Cleanup and refactor some common module code

--- resources/lib/plugin.py
+++ resources/lib/plugin.py
@@ -4,9 +4,16 @@
 # License: GPL v.3 https://www.gnu.org/copyleft/gpl.html
 import sys
 import xbmcplugin
+from xbmcgui import ListItem
 from jurialmunkey.parser import parse_paramstring
-from resources.lib.jsonrpc import get_player_streams
+from resources.lib.jsonrpc import get_jsonrpc
 from resources.lib.method import set_player_subtitle, set_player_audiostream
+
+
+PLAYERSTREAMS = {
+    'audio': {'key': 'audiostreams', 'cur': 'currentaudiostream'},
+    'subtitle': {'key': 'subtitles', 'cur': 'currentsubtitle'}
+}
 
 
 class Plugin(object):
@@ -37,11 +44,41 @@ class Plugin(object):
             return
 
     def get_player_streams(self, stream_type=None, **kwargs):
+        def _get_items(stream_type):
+            def make_item(i):
+                label = i.get("language", "UND")
+                label2 = i.get("name", "")
+                path = f'plugin://script.skinvariables/?info=set_player_streams&stream_index={i.get("index")}&stream_type={stream_type}'
+                infoproperties = {f'{k}': f'{v}' for k, v in i.items() if v}
+                if cur_strm == i.get('index'):
+                    infoproperties['iscurrent'] = 'true'
+                infoproperties['isfolder'] = 'false'
+                listitem = ListItem(label=label, label2=label2, path=path, offscreen=True)
+                listitem.setProperties(infoproperties)
+                return listitem
+
+            ps_def = PLAYERSTREAMS.get(stream_type)
+            if not ps_def:
+                return []
+
+            method = "Player.GetProperties"
+            params = {"playerid": 1, "properties": [ps_def['key'], ps_def['cur']]}
+            response = get_jsonrpc(method, params) or {}
+            response = response.get('result', {})
+            all_strm = response.get(ps_def['key']) or []
+            if not all_strm:
+                return []
+
+            cur_strm = response.get(ps_def['cur'], {}).get('index', 0)
+            return [make_item(i) for i in all_strm if i]
+
         if not stream_type:
             return
+
         items = [
             {'url': li.getPath(), 'listitem': li, 'isFolder': li.getProperty('isfolder').lower() == 'true'}
-            for li in get_player_streams(stream_type) if li]
+            for li in _get_items(stream_type) if li]
+
         self.add_items(items)
 
     def run(self):
