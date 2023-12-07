36bcb535d7d369d5d351f95b69691bccf5407a58
jurialmunkey <jurialmunkey@kodi.tv>
2023-12-07 11:42:35 +1100

    :sparkles: Add default user to list

--- resources/lib/lists/skinusers.py
+++ resources/lib/lists/skinusers.py
@@ -64,7 +64,10 @@ class ListAddSkinUser(Container):
 
 class ListGetSkinUser(Container):
     def get_directory(self, skinid, folder, slug=None, allow_new=False, func=None, **kwargs):
+        import xbmc
+        from jurialmunkey.parser import boolean
         from jurialmunkey.futils import load_filecontent, write_skinfile
+        from resources.lib.shortcuts.futils import reload_shortcut_dir
         from json import loads
 
         filepath = f'{BASEFOLDER}/{skinid}/{USERS_FILE}'
@@ -72,8 +75,10 @@ class ListGetSkinUser(Container):
         meta = loads(file) if file else []
 
         def _login_user():
-            import xbmc
-            user = next(i for i in meta if slug == i.get('slug'))
+            if slug == 'default':
+                user = _get_default_user()
+            else:
+                user = next(i for i in meta if slug == i.get('slug'))
 
             if user.get('code') and str(user.get('code')) != str(Dialog().input('Enter pin code', type=INPUT_NUMERIC)):
                 Dialog().ok('Wrong pin code!', 'Incorrect pin code entered')
@@ -81,27 +86,37 @@ class ListGetSkinUser(Container):
 
             filename = 'script-skinvariables-skinusers.xml'
             content = load_filecontent(f'special://skin/shortcuts/skinvariables-skinusers.xmltemplate')
-            content = content.format(slug=slug, **kwargs)
+            content = content.format(slug=slug if slug != 'default' else '', **kwargs)
             write_skinfile(folders=[folder], filename=filename, content=content)
 
+            import datetime
+            last = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
             xbmc.executebuiltin(f'Skin.SetString(SkinVariables.SkinUser.Name,{user.get("name")})')
-            xbmc.executebuiltin(f'Skin.SetString(SkinVariables.SkinUser,{slug})')
+            xbmc.executebuiltin(f'Skin.SetString(SkinVariables.SkinUser,{slug})' if slug != 'default' else 'Skin.Reset(SkinVariables.SkinUser)')
+            xbmc.executebuiltin(f'Skin.SetString(SkinVariables.SkinUser.{slug}.LastLogin,{last})')
             xbmc.executebuiltin('SetProperty(SkinVariables.SkinUserLogin,True,Home)')
             xbmc.executebuiltin('ReloadSkin()')
+
+        def _get_default_user():
+            return {'name': 'Default User', 'slug': 'default'}
 
         def _make_item(i):
             name = i.get('name') or ''
             slug = i.get('slug') or ''
 
-            if not name or not slug:
+            if not name:
                 return
 
             icon = i.get('icon') or ''
             code = i.get('code') or ''
+            menu = boolean(i.get('menu', True))
             path = f'{BASEPLUGIN}?info=get_skin_user&skinid={skinid}&slug={slug}'
             path = f'{path}&folder={folder}' if folder else path
+            last = xbmc.getInfoLabel(f'Skin.String(SkinVariables.SkinUser.{slug}.LastLogin)') or 'Never logged in'
 
-            li = ListItem(label=name, label2=slug, path=path)
+            li = ListItem(label=name, label2=last, path=path)
+            li.setProperty('last', last)
             li.setProperty('slug', slug)
             li.setProperty('code', code) if code else None
             li.setArt({'thumb': icon, 'icon': icon}) if icon else None
@@ -109,12 +124,11 @@ class ListGetSkinUser(Container):
             li.addContextMenuItems([
                 ('Rename', f'RunPlugin({path}&func=rename)'),
                 ('Delete', f'RunPlugin({path}&func=delete)')
-            ])
+            ]) if menu else None
 
             return (path, li, False)
 
         def _join_item():
-            from jurialmunkey.parser import boolean
             if not boolean(allow_new):
                 return []
             name = 'Add new user...'
@@ -124,13 +138,15 @@ class ListGetSkinUser(Container):
             return [(path, li, False)]
 
         def _open_directory():
-            items = [j for j in (_make_item(i) for i in meta) if j] + _join_item()
+            items = []
+            if xbmc.getCondVisibility('!Skin.HasSetting(SkinVariables.SkinUsers.DisableDefaultUser)'):
+                items += [_make_item(_get_default_user())]
+            items += [j for j in (_make_item(i) for i in meta) if j] + _join_item()
             plugin_category = ''
             container_content = ''
             self.add_items(items, container_content=container_content, plugin_category=plugin_category)
 
         def _delete_user():
-            from resources.lib.shortcuts.futils import reload_shortcut_dir
             x, user = next((x, i) for x, i in enumerate(meta) if slug == i.get('slug'))
 
             if user.get('code') and str(user.get('code')) != str(Dialog().input('Enter pin code', type=INPUT_NUMERIC)):
@@ -144,7 +160,6 @@ class ListGetSkinUser(Container):
             reload_shortcut_dir()
 
         def _rename_user():
-            from resources.lib.shortcuts.futils import reload_shortcut_dir
             x, user = next((x, i) for x, i in enumerate(meta) if slug == i.get('slug'))
 
             if user.get('code') and str(user.get('code')) != str(Dialog().input('Enter pin code', type=INPUT_NUMERIC)):
